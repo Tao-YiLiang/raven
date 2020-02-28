c31c27c83f74e81b400376470faf1582c6d2b5c3
Abhinav Gairola <abhinav>
2020-02-27 19:26:03 -0700

    SPSA.py

--- framework/Optimizers/gradients/SPSA.py
+++ framework/Optimizers/gradients/SPSA.py
@@ -13,28 +13,13 @@ from utils import InputData, InputTypes, randomUtils, mathUtils
 
 
 
-"Author:--"
+"Author: gairabhi"
 
-"Strictly as per the guidelines given in the"
-#https://www.jhuapl.edu/SPSA/PDF-SPSA/Matlab-Auto_gain_sel-b-w.pdf#
-
-#https://www.jhuapl.edu/SPSA/PDF-SPSA/Matlab-Auto_gain_sel-b-w.pdf#
 
 
 class SPSA(GradientApproximater):
-  ### Some global parameters ###
-  
-  counter = 0
-  ak = None
-  
-  a = 0.001
-  A = 0.5
-  alpha = 0.602
-  ck = None
-  c = None
-  gamma = 0.101
+
   def chooseEvaluationPoints(self, opt, stepSize):
-    self.counter+=1
     """
       Determines new point(s) needed to evaluate gradient
       @ In, opt, dict, current opt point (normalized)
@@ -42,63 +27,29 @@ class SPSA(GradientApproximater):
       @ Out, evalPoints, list(dict), list of points that need sampling
       @ Out, evalInfo, list(dict), identifying information about points
     """
-    dh = self._proximity * stepSize 
-    self.c = dh
-    self.ak = self.a/np.power(self.counter+self.A, self.alpha)
-    self.ck=self.c/np.power(self.counter,self.gamma)
-    perturb = randomUtils.randPointsOnHypersphere(2)
- 
-    
-    
+    dh = self._proximity * stepSize
+    perturb = randomUtils.randPointsOnHypersphere(self.N)
+    delta = {}
+    new = {}
+    for i,var in enumerate(opt.keys()):
+      if var != 'ans':
+        delta[var] = perturb[i]*dh
+        
+        new[var] = opt[var]+delta[var]
 
     evalPoints = []
-    evalInfo = []
+    evalInfo = []  
 
-    delta=[]
-    
-   
-
-
-    for o, optVar in enumerate(self._optVars):
-
-      
-      
-
-   
-
- 
-      optValue = opt[optVar]
-      new = copy.deepcopy(opt)
-      new2 = copy.deepcopy(opt)
-
-
- 
-
-      
-      
-      delta.append( self.ck*perturb[o] )
-    
-    
-      
-      new[optVar] = optValue + self.ck*perturb[o]
-      new2[optVar] = optValue - self.ck*perturb[o]
-   
-   
-
-      evalPoints.append(new)
-      evalPoints.append(new2)
-      evalInfo.append({'type': 'grad',
-                       'optVar': optVar,
-                       'delta': delta})
-
-      evalInfo.append({'type': 'grad',
-                       'optVar': optVar,
-                       'delta': delta})
-
+    evalPoints.append(new)
   
-      
-    return evalPoints, evalInfo
+    
+    evalInfo.append({'type': 'grad',
+                       
+                       'delta': delta})
 
+
+    return evalPoints, evalInfo       
+  
   def evaluate(self, opt, grads, infos, objVar):
     """
       Approximates gradient based on evaluated points.
@@ -110,52 +61,29 @@ class SPSA(GradientApproximater):
       @ Out, direction, dict, versor (unit vector) for gradient direction
     """
 
+    delta = infos[0]['delta']
 
     gradient = {}
 
- 
+    lossDiff = np.atleast_1d(mathUtils.diffWithInfinites(grads[0][objVar],opt[objVar]))
+      
+    for var in grads[0].keys():
+      if var != objVar:
+        gradient [var] = lossDiff/(delta[var])
+   
     
-
-    for i in range(0,len(grads),2):
-
-     
-      
-
-      info = infos[i]
-
-      if i == 0:
-        delta = info['delta'][i]
-      if i == 2:
-        delta = info['delta'][i-1]
-      activeVar = info['optVar']
-
-    
-
-      
-      lossDiff = (grads[i][objVar]-grads[i+1][objVar])
-
-      grad = lossDiff/(2*delta)
-
-     
-
-
-      
-  
-      gradient[activeVar] = grad
-  
   
     magnitude, direction, foundInf = mathUtils.calculateMagnitudeAndVersor(list(gradient.values()))
-
+    
     direction = dict((var, float(direction[v])) for v, var in enumerate(gradient.keys()))
-
+ 
     return magnitude, direction, foundInf
 
   def numGradPoints(self):
     """
       Returns the number of grad points required for the method
     """
-
-    return self.N*2
+    return self.N/self.N
   
 
 
