223040bb8c93a097fdf77d129a8fff78e8d18440
Paul W. Talbot <paul.talbot@inl.gov>
2020-03-03 12:38:37 -0700

    max and multitraj added, but need to kill off multitraj

--- framework/Optimizers/Optimizer.py
+++ framework/Optimizers/Optimizer.py
@@ -268,6 +268,16 @@ class Optimizer(AdaptiveSampler):
       @ Out, convergence, bool, variable indicating whether the convergence criteria has been met.
     """
 
+  @abc.abstractmethod
+  def _updateSolutionExport(self, traj, rlz, acceptable):
+    """
+      Stores information to the solution export.
+      @ In, traj, int, trajectory which should be written
+      @ In, rlz, dict, collected point
+      @ In, acceptable, bool, acceptability of opt point
+      @ Out, None
+    """
+
   def _addTrackingInfo(self, info, **kwargs):
     """
       Creates realization identifiers to identifiy particular realizations as they return from the JobHandler.
